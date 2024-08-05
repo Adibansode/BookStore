4e5ebcc2bb472a83985817c85c015adad2e52d67
Adibansode <adityabansode99@gmail.com>
2024-08-05 12:28:13 +0530

    Added commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,6 +13,8 @@ function App() {
   return (
     <>
     <div className="dark:bg-slate-900 dark:text-white">
+
+      {/* BrowserRoutes here */}
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/course" element={authUser? <Courses></Courses>:<Navigate to="/signup"></Navigate> }></Route>
